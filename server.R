3dd134c2ecebb91285d3c1424adfd9fc8c1b1264
unknown <JvandeGevel@NLLT006100.nl.deloitte.com>
2016-05-08 12:21:34 +0200

    First commit

---
+++ server.R
@@ -0,0 +1,33 @@
+library(shiny)
+
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+          "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+
+shinyServer(function(input, output, session) {
+  
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    iris[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+  })
+  
+  output$plot2 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData())
+    abline(lm(iris[,input$ycol] ~ iris[,input$xcol] ))
+  })
+  
+  
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel('Iris dataset exploration'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(iris)),
+    selectInput('ycol', 'Y Variable', names(iris),
+                selected=names(iris)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 9)
+  ),
+  mainPanel(
+    plotOutput('plot1'),
+    plotOutput('plot2')
+  )
+))
